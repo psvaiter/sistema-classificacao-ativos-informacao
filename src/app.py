686a1f039b7d0766a13753e83e55785fadb67760
Patrick Svaiter <psvaiter@gmail.com>
2018-02-21 01:18:16 -0300

    Reorganiza arquivo principal app.py habilitando servidor WSGI de referência.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,26 @@
 import falcon
 import app_setup as setup
-# from wsgiref.simple_server import make_server
+from wsgiref import simple_server
 
+# Some WSGI servers expect the WSGI application
+# to be named 'application' by default.
 
 api = application = falcon.API()
 
 setup.configure_media_handlers(api)
 setup.configure_routes(api)
 
-# if __name__ == '__main__':
-#     httpd = make_server('', 8000, api)
-#     print('Serving on port 8000...')
-#
-#     httpd.serve_forever()
+if __name__ == '__main__':
+
+    # Use this server as the last resort, even for debug use because
+    # it's very (very) slow.
+    # As a Windows alternative, run in terminal:
+    #   > waitress-serve --port=8000 app:api
+    # As a Linux alternative, run in terminal:
+    #   > gunicorn --reload api
+
+    host = ''
+    port = 8000
+    with simple_server.make_server(host, port, api) as httpd:
+        print('Serving on port {}...'.format(port))
+        httpd.serve_forever()
