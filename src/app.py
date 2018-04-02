d6df2cbc5bfe9af21df866751674cce2623a8a1b
Patrick Svaiter <psvaiter@gmail.com>
2018-04-02 03:58:23 -0300

    Habilita CORS (Cross-Origin Resource Sharing) sem restrições.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 import falcon
 import app_setup
 from wsgiref import simple_server
+from falcon_cors import CORS
+
+cors = CORS(allow_all_origins=True,
+            allow_all_headers=True,
+            allow_all_methods=True)
 
 # Some WSGI servers expect the WSGI application to be named 'application' by default.
 # Then we defined 'api' as a shorter option.
-api = application = falcon.API()
+api = application = falcon.API(
+    middleware=[cors.middleware]
+)
 
 app_setup.configure_media_handlers(api)
 app_setup.configure_routes(api)
