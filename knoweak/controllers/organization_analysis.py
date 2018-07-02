f83582167a90dfd893aebb88a46aa5d69b233be5
Patrick Svaiter <psvaiter@gmail.com>
2018-07-01 21:30:56 -0300

    Adds skeleton of organization analysis responders.

--- knoweak/controllers/organization_analysis.py
+++ knoweak/controllers/organization_analysis.py
@@ -1,11 +1,56 @@
 import falcon
 
+from .utils import get_collection_page
+from knoweak.models import Session, Organization
+
 
 class Collection:
     """GET and POST organization analyses."""
 
     def on_get(self, req, resp, organization_code):
-        pass
+        """GETs a paged collection of analyses of an organization.
+
+        :param req: See Falcon Request documentation.
+        :param resp: See Falcon Response documentation.
+        :param organization_code: The code of the organization.
+        """
+        session = Session()
+        try:
+            item = session.query(Organization).get(organization_code)
+            if item is None:
+                raise falcon.HTTPNotFound()
+
+            # code here
+            query = None
+
+            data, paging = get_collection_page(req, query)
+            resp.media = {
+                'data': data,
+                'paging': paging
+            }
+        finally:
+            session.close()
 
     def on_post(self, req, resp, organization_code):
-        pass
+        """Creates a new analysis for the organization considering the already filled values
+        for relevance, vulnerability and security threat levels in processes, IT services,
+        IT assets and security threats.
+
+        :param req: See Falcon Request documentation.
+        :param resp: See Falcon Response documentation.
+        :param organization_code: The code of the organization.
+        """
+        session = Session()
+        try:
+            item = session.query(Organization).get(organization_code)
+            if item is None:
+                raise falcon.HTTPNotFound()
+
+            # code here
+
+            session.commit()
+            resp.status = falcon.HTTP_CREATED
+            resp.location = req.relative_uri + f'/{item.id}'
+            resp.media = {'data': item.asdict()}
+        finally:
+            session.close()
