a16f9f5395bfe56a2f4ebe86906e0d5d66d7c9a5
Patrick Svaiter <psvaiter@gmail.com>
2018-07-23 17:20:04 -0300

    Puts version in __init__.py.

--- knoweak/api/resources/system.py
+++ knoweak/api/resources/system.py
@@ -1,6 +1,9 @@
+import knoweak
+
+
 class AppInfo:
 
     def on_get(self, req, resp):
         resp.media = {
-            'version': '1.0.0-beta'
+            'version': knoweak.__version__
         }
