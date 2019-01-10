b7a0ebbbc1e4193d1e61336069c888fd17e12a5c
Patrick Svaiter <psvaiter@gmail.com>
2019-01-10 03:11:14 -0200

    Adds validation of token scopes.

--- knoweak/api/middlewares/oauth2.py
+++ knoweak/api/middlewares/oauth2.py
@@ -1,10 +1,15 @@
 import falcon
+import jwt
+
 from knoweak.settings import AUTH
 
 
 class OAuth2:
 
-    def process_request(self, req, resp):
+    def __init__(self):
+        pass
+
+    def process_resource(self, req, resp, resource, params):
         if AUTH['disabled']:
             return
 
@@ -17,3 +22,24 @@ class OAuth2:
             raise falcon.HTTPUnauthorized(description='Invalid authentication scheme. Expected value: Bearer.')
         if len(auth_header_parts) == 1:
             raise falcon.HTTPUnauthorized(description='Missing Bearer token.')
+
+        try:
+            decoded_token = jwt.decode(auth_header_parts[1], key='', algorithms='HS256')
+        except Exception as e:
+            raise falcon.HTTPUnauthorized(description=f'Invalid authentication token. {str(e)}.')
+
+        self._validate_scopes(resource, decoded_token)
+
+    def _validate_scopes(self, resource, decoded_token):
+        # Read the scopes from token. It can be either a string or a list.
+        token_scopes = decoded_token.get('scopes', [])
+        if isinstance(token_scopes, str):
+            token_scopes = token_scopes.split(' ')
+        elif not isinstance(token_scopes, list):
+            token_scopes = []
+
+        # Validate against scopes registered in resource.
+        # If a resource does not have at least one scope, it doesn't have any access restrictions.
+        allowed_scopes = getattr(resource, 'auth', {}).get('allowed_scopes', [])
+        if allowed_scopes and not set(allowed_scopes).issubset(token_scopes):
+            raise falcon.HTTPForbidden(description='Check your permissions (scopes).')
