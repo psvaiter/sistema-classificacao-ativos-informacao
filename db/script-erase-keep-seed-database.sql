0e1d4bf89817062ab52829b23c21090aee65ab85
Patrick Svaiter <psvaiter@gmail.com>
2019-02-15 23:15:28 -0200

    Adds scripts to clear database.

---
+++ db/script-erase-keep-seed-database.sql
@@ -0,0 +1,12 @@
+-- Erase organizations
+delete from organization_analysis where organization_analysis_id > 0;
+delete from organization where organization_id > 0;
+
+-- Erase catalog
+delete from business_department where business_department_id > 0;
+delete from business_macroprocess where business_macroprocess_id > 0;
+delete from business_process where business_process_id > 0;
+delete from it_service where it_service_id > 0;
+delete from it_asset where it_asset_id > 0;
+delete from security_threat where security_threat_id > 0;
+delete from mitigation_control where mitigation_control_id > 0;
