b25a59f93bf5d432cf3bdb662b664e991f65f6d3
Patrick Svaiter <psvaiter@gmail.com>
2019-03-17 20:34:50 -0300

    Format script to insert seed data.

--- db/scripts/init/02-insert-seed-data.sql
+++ db/scripts/init/02-insert-seed-data.sql
@@ -1,18 +1,18 @@
 USE `knoweak`;
 
-set @ts = current_timestamp;
-insert into it_asset_category 
-values 
-	(1, 'Informação', @ts, @ts), 				-- Information
+SET @ts = CURRENT_TIMESTAMP;
+INSERT INTO it_asset_category
+VALUES
+    (1, 'Informação', @ts, @ts), 				-- Information
     (2, 'Política', @ts, @ts), 					-- Policy
     (3, 'Pessoa', @ts, @ts),					-- People
     (4, 'Ambiente / Infraestrutura', @ts, @ts),	-- Environment / Infrastructure
     (5, 'Hardware', @ts, @ts),
     (6, 'Software', @ts, @ts);
 
-insert into rating_level
-values
-	(1, 'Muito baixo'),
+INSERT INTO rating_level
+VALUES
+    (1, 'Muito baixo'),
     (2, 'Baixo'),
     (3, 'Médio'),
     (4, 'Alto'),
