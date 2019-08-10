62723133595829230e5b589431a32cda3b092460
Patrick Svaiter <psvaiter@gmail.com>
2019-08-10 16:39:31 -0300

    Improve comment.

--- knoweak/app.py
+++ knoweak/app.py
@@ -1,16 +1,17 @@
 from knoweak.setup import get_api
 from wsgiref import simple_server
 
-# Some WSGI servers expect the WSGI application to be named 'application' by default.
-# Then we defined 'api' as a shorter option too.
+# Some WSGI servers expect the WSGI application to be named 'application'
+# by default. Then we defined 'api' as a shorter option too.
 api = application = get_api()
 
 if __name__ == '__main__':
 
-    # Use this server as the last resort, even for debugging because it's very (very) slow.
-    # As a Windows alternative, run in terminal:
+    # Use this simple server as the last resort, even for debugging
+    # because it's very (very) slow.
+    # In Windows, run in terminal:
     #   > waitress-serve --port=8000 app:api
-    # As a Linux alternative, run in terminal:
+    # In Linux, run in terminal:
     #   > gunicorn --reload api
 
     host = ''
